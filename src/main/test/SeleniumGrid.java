468c7b5a731ba3c8cbcd6d13d8d155cb9ff2802c
Administrator <admin@example.com>
2021-02-24 06:04:49 +0000

    Update SeleniumGrid.java

--- src/main/test/SeleniumGrid.java
+++ src/main/test/SeleniumGrid.java
@@ -16,7 +16,7 @@
      public void setUp() throws MalformedURLException {
          baseURL = "https://www.w3schools.com/java/default.asp";
          nodeURL = "http://selenium-hub.hema.svc.cluster.local:4444/wd/hub";
-         DesiredCapabilities capability = DesiredCapabilities.chrome();
+         ChromeOptions options = new ChromeOptions();
          capability.setBrowserName("chrome");
          capability.setPlatform(Platform.LINUX);
          System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
