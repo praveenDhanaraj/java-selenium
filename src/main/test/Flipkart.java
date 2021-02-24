6370e55ac424063d46edc2cf3e789a0c7f3ee42a
Administrator <admin@example.com>
2021-02-24 06:05:12 +0000

    Update Flipkart.java

--- src/main/test/Flipkart.java
+++ src/main/test/Flipkart.java
@@ -33,7 +33,7 @@ public class Flipkart {
 public void setUp() throws MalformedURLException {
          baseURL = "https://www.flipkart.com";
          nodeURL = "http://selenium-hub.hema.svc.cluster.local:4444/wd/hub";
-         DesiredCapabilities capability = DesiredCapabilities.chrome();
+         ChromeOptions options = new ChromeOptions();
          capability.setBrowserName("chrome");
          capability.setPlatform(Platform.LINUX);
          System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
