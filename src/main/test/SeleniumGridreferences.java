02a71d3069cb5995d9c05f2cfd16cccd0ac54f6e
Administrator <admin@example.com>
2021-03-08 06:37:54 +0000

    Add new file

---
+++ src/main/test/SeleniumGridreferences.java
@@ -0,0 +1,46 @@
+ package com.objectrepository.demo;
+ import org.openqa.selenium.*;
+ import org.openqa.selenium.remote.DesiredCapabilities;
+ import java.net.MalformedURLException;
+ import java.net.URL;
+ import org.openqa.selenium.remote.RemoteWebDriver;
+ import org.testng.Assert;
+ import org.testng.annotations.*;
+ import java.util.concurrent.TimeUnit;
+ 
+ public class SeleniumGridreferences {
+
+     WebDriver driver;
+     String baseURL, nodeURL;
+
+     @BeforeClass
+     public void setUp() throws MalformedURLException {
+         baseURL = "https://www.w3schools.com/java/default.asp";
+         nodeURL = "http://zalenium.hema.svc.cluster.local:4444/wd/hub";
+         DesiredCapabilities capability = DesiredCapabilities.chrome();
+         capability.setPlatform(Platform.LINUX);
+         driver = new RemoteWebDriver(new URL(nodeURL), capability);
+         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);         
+     }
+
+     @AfterClass
+     public void afterTest() {
+         driver.quit();
+     }
+     @Test
+    public void clickreferences() {
+    try
+    {
+    Thread.sleep(4000); 
+    
+    
+        driver.get(baseURL);
+        driver.findElement(By.id("topnavbtn_references")).click();
+    
+    }
+    catch(Exception e)
+    {
+    
+    }
+    }
+	 }
