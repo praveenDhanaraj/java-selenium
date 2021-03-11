75ad9a871713c93b6c7f91ea23bf13aaacff18b4
Administrator <admin@example.com>
2021-03-11 19:29:38 +0530

    Update Flipkartlogout.java

--- src/main/test/Flipkartlogout.java
+++ src/main/test/Flipkartlogout.java
@@ -21,7 +21,10 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class Flipkartlogout {
-         String baseURL;
+    private String baseURL;
+    private WebDriver driver;
+
+
 
 
          String username = ""; // Change to your username and passwrod
@@ -32,9 +35,10 @@ public class Flipkartlogout {
 public void setUp() throws MalformedURLException {
          baseURL = "https://www.flipkart.com";
          System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/testing/chromedriver");
-         ChromeOptions chromeOptions = new ChromeOptions();
-         chromeOptions.addArguments("--headless");
-         WebDriver driver = new ChromeDriver(chromeOptions);
+         driver = new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.get(baseURL);
+
 }
 
 
