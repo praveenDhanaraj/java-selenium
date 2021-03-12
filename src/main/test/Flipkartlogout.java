9383f4feb86aceebbb588aff3409aac01a7a678e
Administrator <admin@example.com>
2021-03-12 09:38:05 +0530

    Update Flipkartlogout.java

--- src/main/test/Flipkartlogout.java
+++ src/main/test/Flipkartlogout.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
-public class Flipkart {
+public class Flipkartlogout {
         private String baseURL;
         private WebDriver driver;
     ChromeDriverService service;
