80b8d6acbe0a9e091f8ec649108bfe0053a67f87
Administrator <admin@example.com>
2021-03-12 09:32:12 +0530

    Update Flipkart.java

--- src/main/test/Flipkart.java
+++ src/main/test/Flipkart.java
@@ -1,53 +1,47 @@
 package com.sampletestpackage;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.net.URL;
 import java.net.MalformedURLException;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
-import org.testng.annotations.*;
 
 public class Flipkart {
         private String baseURL;
         private WebDriver driver;
+    ChromeDriverService service;
 
 
          String username = ""; // Change to your username and passwrod
          String password = "";
 
 // This method is to navigate flipkart URL
-@BeforeClass
-public void setUp() throws MalformedURLException {
+@BeforeClass(alwaysRun = true)
+public void setUp() throws IOException {
          baseURL = "https://www.flipkart.com";
-         System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/testing/chromedriver");
-         driver = new ChromeDriver();
-         driver.manage().window().maximize();
-         driver.get(baseURL);
+        service = new ChromeDriverService.Builder().usingDriverExecutable(new File("/var/lib/jenkins/testing/chromedriver")).usingAnyFreePort().build();
+        service.start();
+    driver = new RemoteWebDriver(service.getUrl(),new ChromeOptions());
 }
 
 
 // To log in flipkart
 @Test
 public void login() {
-try
-{
-Thread.sleep(1000);
-}
-catch(Exception e)
-{
+    driver.manage().window().maximize();
+    driver.get(baseURL);
+
+
 driver.findElement(By.partialLinkText("Login")).click();
 driver.findElement(
 By.cssSelector("input[placeholder='Enter email/mobile']"))
@@ -57,10 +51,10 @@ By.cssSelector("input[placeholder='Enter password']"))
 .sendKeys(password);
 driver.findElement(By.cssSelector("input[value='Login'][class='submit-btn login-btn btn']")).click();
 }
-}
 
 @AfterClass
 public void quit() {
+    service.stop();
 driver.quit();
 }
 }
